9772f7fc9afe1b6aea3e157092f4d50ade31ea1e
Leon Li <lilei.leon@gmail.com>
2016-06-13 18:36:08 +0800

    correct the start shell

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 chown -R ubuntu:ubuntu /opt/node_home
-su - ubuntu -c "export NODE_ENV=production"
-su - ubuntu -c "pm2 start /opt/node_home/server.js --name=beyond"
+su - ubuntu -c "NODE_ENV=production pm2 start /opt/node_home/server.js --name=beyond"
