6573ffe5d7d77566101900819844d15eaaa8e477
Leon Li <lilei.leon@gmail.com>
2016-06-13 18:38:38 +0800

    update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 chown -R ubuntu:ubuntu /opt/node_home
-su - ubuntu -c "NODE_ENV=production pm2 start /opt/node_home/server.js --name=beyond"
+su - ubuntu -c "cd /opt/node_home && NODE_ENV=production pm2 start /opt/node_home/server.js --name=beyond"
